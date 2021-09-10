af91ef24d73238014dde91a6565392ab429ba9e3
Gray007 <cmzlelicanin@gmail.com>
2021-09-10 18:34:51 +0200

    3C - Verifying Front & Back Integration

--- index.js
+++ index.js
@@ -1,100 +1,53 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const Note = require('./models/note')
 
-app.use(express.json())
-app.use(cors())
 app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
-
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-  
-  app.get('/api/notes', (request, response) => {
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
     response.json(notes)
   })
+})
 
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(204).end()
-    }
-  })
-
-  // Debugging 
-  // app.get('/api/notes/:id', (request, response) => {
-  //   const id = request.params.id
-  //   const note = notes.find(note => {
-  //     console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-  //     return note.id === id
-  //   })
-  //   console.log(request.headers)
-  //   response.json(note)
-  // })
-
-
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  
+  if (!body.content === undefined) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
   }
 
-  app.post('/api/notes', (request, response) => {
+  const note = new Note ({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
 
-    const body = request.body
-    
-    if (!body.content) {
-      return response.status(400).json({
-        error: 'content missing'
-      })
-    }
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
 
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
-    }
-
-    notes = notes.concat(note)
-
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
   })
-  
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
+})
 
-  const PORT = process.env.PORT || 3001
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
